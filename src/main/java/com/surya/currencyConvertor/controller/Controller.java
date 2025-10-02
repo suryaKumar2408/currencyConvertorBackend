7c55c30181fd3cef3e0b60ad9c212233f77567e2
suryaKumar2408 <suryashukla3284@gmail.com>
2025-10-02 19:51:09 +0530

    Update frontend script.js with backend URL and backend CORS config

--- src/main/java/com/surya/currencyConvertor/controller/Controller.java
+++ src/main/java/com/surya/currencyConvertor/controller/Controller.java
@@ -10,7 +10,6 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class Controller {
     private static final Map<String ,Double> exchangeRate=new HashMap<>();
     static {
